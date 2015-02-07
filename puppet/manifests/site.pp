a03585d4075f8594067611d1a60c30597595512e
Romain Yon <romain.yon@gmail.com>
2015-02-08 00:41:59 +0100

    play with puppet

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -9,6 +9,17 @@ apt::source { 'elasticsearch':
     include_src => false,
 }
 
+apt::source { 'logstash':
+    location    => 'http://packages.elasticsearch.org/logstash/1.4/debian',
+    release     => 'stable',
+    repos       => 'main',
+    key         => 'D88E42B4',
+
+    # Elasticsearch doesn't publish their sources into the apt repository, don't
+    # include them or you will get errors when trying to run `apt-get update`
+    include_src => false,
+}
+
 class { 'elasticsearch':
   version => '1.4.2'
 }
