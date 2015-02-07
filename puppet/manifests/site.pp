74b5772308d759cd00f8d75fbdf1bfe9a740d434
Romain Yon <romain.yon@gmail.com>
2015-02-08 00:39:37 +0100

    play with puppet

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,10 +1,13 @@
 apt::source { 'elasticsearch':
-  location   => 'http://packages.elasticsearch.org/elasticsearch/1.4/debian',
-  release    => 'stable',
-  repos      => 'main',
-  key        => 'D88E42B4',
-  key_server => 'pgp.mit.edu',
-  }
+    location    => 'http://packages.elasticsearch.org/elasticsearch/1.4/debian',
+    release     => 'stable',
+    repos       => 'main',
+    key         => 'D88E42B4',
+
+    # Elasticsearch doesn't publish their sources into the apt repository, don't
+    # include them or you will get errors when trying to run `apt-get update`
+    include_src => false,
+}
 
 class { 'elasticsearch':
   version => '1.4.2'
