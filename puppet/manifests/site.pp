79dc596f90e7f6792af68074bbc4879a9ede48a8
Romain Yon <romain.yon@gmail.com>
2015-02-08 00:17:56 +0100

    play with puppet

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,3 +1,11 @@
+apt::source { 'elasticsearch':
+  location          => 'http://packages.elasticsearch.org/elasticsearch/1.4/debian',
+  release           => 'stable',
+  repos             => 'main',
+  key               => 'D88E42B4',
+  key_server        => 'https://packages.elasticsearch.org/GPG-KEY-elasticsearch'
+}
+
 class { 'elasticsearch':
   version => '1.4.2'
 }
